f1289e266c4d01cd625b9e758d988225809297a2
Paul <paul@Paul-6.local>
2018-07-07 17:27:43 +0800

    画登录界面

--- GameBox/LoginPage/Controller/LoginViewController.m
+++ GameBox/LoginPage/Controller/LoginViewController.m
@@ -7,8 +7,17 @@
 //
 #import "LoginViewController.h"
 #import "PopTool.h"
-@interface LoginViewController ()
+
+#define WIDTH_PERCENT  [UIScreen mainScreen].bounds.size.width/375.0
+#define HEIGHT_PERCENT [UIScreen mainScreen].bounds.size.height/667.0
+#define SCREEN_WIDTH  [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT  [UIScreen mainScreen].bounds.size.height
+
+@interface LoginViewController ()<UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UIView *leftContrainerView;
+@property (weak, nonatomic) IBOutlet UITextField *account_textField;
+@property (weak, nonatomic) IBOutlet UITextField *password_textField;
+@property (weak, nonatomic) IBOutlet UITextField *check_textField;
 
 @end
 
@@ -22,12 +31,18 @@
 -(void)configurationUI{
     UIImage  * img = [UIImage  imageNamed:@"left_bg"];
     self.leftContrainerView.layer.contents = (__bridge id _Nullable)(img.CGImage);
+    UITapGestureRecognizer * tap = [[UITapGestureRecognizer  alloc] initWithTarget:self action:@selector(closeKeyboard)];
+    tap.delegate= self;
+    [self.view addGestureRecognizer:tap];
+}
+-(void)closeKeyboard{
+    [self.view endEditing:YES];
 }
 - (IBAction)btnClick:(UIButton *)sender {
     
 }
 -(void)show{
-    self.view.frame = CGRectMake(0, 0, 411, 268);
+    self.view.frame = CGRectMake(0, 0, 411, 250);
     [[PopTool sharedInstance] showWithPresentView:self.view subTitle:@"登录" AnimatedType:AnimationTypeScale AnimationDirectionType:AnimationDirectionFromLeft];
 }
 - (void)didReceiveMemoryWarning {
