c4d092c470d05878eed3ba97b9172a99f0d6fb09
sam <sam@sam.local>
2018-07-10 11:46:31 +0800

    添加系统公告和游戏公告界面

--- GameBox/Promo/View/SH_SystemNotification.m
+++ GameBox/Promo/View/SH_SystemNotification.m
@@ -1,25 +1,20 @@
 //
-//  SH_GameAnnouncementView.m
+//  SH_SystemNotification.m
 //  GameBox
 //
-//  Created by sam on 2018/7/9.
+//  Created by sam on 2018/7/10.
 //  Copyright © 2018年 shin. All rights reserved.
 //
 
-#import "SH_GameAnnouncementView.h"
+#import "SH_SystemNotification.h"
 #import <Masonry/Masonry.h>
 #import "SH_PromoViewCell.h"
 
-@interface SH_GameAnnouncementView ()<UITableViewDataSource, UITableViewDelegate>
-@property (weak, nonatomic) IBOutlet UIView *view1;
-@property (weak, nonatomic) IBOutlet UIView *view2;
-@property (weak, nonatomic) IBOutlet UIView *view3;
-
+@interface SH_SystemNotification () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
 @end
 
-@implementation SH_GameAnnouncementView
+@implementation SH_SystemNotification
 
 -(void)awakeFromNib {
     [super awakeFromNib];
