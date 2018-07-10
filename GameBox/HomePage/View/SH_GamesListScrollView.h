9fb94d9ed8b170fdb2a2c5a4b74faf5501e9b620
shin <shin@dawoo.com>
2018-07-10 19:07:15 +0800

    增加首页游戏列表

--- GameBox/HomePage/View/SH_GamesListScrollView.h
+++ GameBox/HomePage/View/SH_GamesListScrollView.h
@@ -8,6 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SH_GamesListScrollView : UIScrollView
+@class SH_GamesListScrollView;
+
+@protocol GamesListScrollViewDataSource <NSObject>
+
+@required
+- (NSInteger)numberOfItemsOfGamesListScrollView:(SH_GamesListScrollView *)scrollView;
+- (UIView *)gamesListScrollView:(SH_GamesListScrollView *)scrollView viewForItem:(NSInteger)index;
+
+@end
+
+@protocol GamesListScrollViewDelegate <NSObject>
+
+@optional
+- (void)gamesListScrollView:(SH_GamesListScrollView *)scrollView didSelectItem:(NSInteger)index;
+
+@end
+
+@interface SH_GamesListScrollView : UIView
+
+@property (nonatomic, weak) id <GamesListScrollViewDataSource> dataSource;
+@property (nonatomic, weak) id <GamesListScrollViewDelegate> delegate;
+
+- (void)reloaData;
 
 @end
--- GameBox/HomePage/View/SH_GamesListScrollView.m
+++ GameBox/HomePage/View/SH_GamesListScrollView.m
@@ -7,15 +7,147 @@
 //
 
 #import "SH_GamesListScrollView.h"
+#import <Masonry/Masonry.h>
+#import "SH_GameItemView.h"
+
+#define SH_GAMELIST_ITEM_WIDTH 125
+#define SH_GAMELIST_ITEM_HEIGHT 108
+
+@interface SH_GamesListScrollView () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIButton *preBT;
+@property (nonatomic, strong) UIButton *nextBT;
+@property (nonatomic, assign) int currentPage;
+
+@end
 
 @implementation SH_GamesListScrollView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)reloaData
+{
+    NSInteger itemsNum = [self.dataSource numberOfItemsOfGamesListScrollView:self];
+    CGFloat calculationW = (itemsNum/2+itemsNum%2)*SH_GAMELIST_ITEM_WIDTH;
+    CGFloat contentSizeW = calculationW > self.scrollView.frame.size.width ? calculationW : self.scrollView.frame.size.width;
+    
+    [self.scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
+    if (calculationW > self.scrollView.frame.size.width) {
+        self.nextBT.hidden = NO;
+        self.preBT.hidden = YES;
+    }
+    else
+    {
+        self.nextBT.hidden = YES;
+        self.preBT.hidden = YES;
+    }
+
+    self.scrollView.contentSize = CGSizeMake(contentSizeW, self.scrollView.frame.size.height);
+    for (int i = 0; i < itemsNum; i++) {
+        SH_GameItemView *gameItemView = (SH_GameItemView *)[self.dataSource gamesListScrollView:self viewForItem:i];
+        [self.scrollView addSubview:gameItemView];
+        [gameItemView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo((i/2)*SH_GAMELIST_ITEM_WIDTH);
+            make.top.mas_equalTo((i%2)*SH_GAMELIST_ITEM_HEIGHT);
+            make.width.mas_equalTo(SH_GAMELIST_ITEM_WIDTH);
+            make.height.mas_equalTo(SH_GAMELIST_ITEM_HEIGHT);
+        }];
+    }
 }
-*/
+
+- (UIScrollView *)scrollView
+{
+    if (_scrollView == nil) {
+        _scrollView = [[UIScrollView alloc] init];
+        _scrollView.delegate = self;
+        _scrollView.pagingEnabled = YES;
+        _scrollView.showsVerticalScrollIndicator = NO;
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        [self addSubview:_scrollView];
+        [_scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.right.top.bottom.equalTo(self);
+        }];
+        [self layoutIfNeeded];
+    }
+    return _scrollView;
+}
+
+- (UIButton *)nextBT
+{
+    if (_nextBT == nil) {
+        _nextBT = [UIButton buttonWithType:UIButtonTypeCustom];
+        _nextBT.hidden = YES;
+        [_nextBT setImage:[UIImage imageNamed:@"arrow_next"] forState:UIControlStateNormal];
+        [_nextBT addTarget:self action:@selector(nextBTClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_nextBT];
+        [_nextBT mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(self.mas_centerY);
+            make.width.mas_equalTo(38);
+            make.height.mas_equalTo(46);
+            make.right.equalTo(self.mas_right).mas_equalTo(-12);
+        }];
+    }
+    return _nextBT;
+}
+
+- (UIButton *)preBT
+{
+    if (_preBT == nil) {
+        _preBT = [UIButton buttonWithType:UIButtonTypeCustom];
+        _preBT.hidden = YES;
+        [_preBT setImage:[UIImage imageNamed:@"arrow_prev"] forState:UIControlStateNormal];
+        [_preBT addTarget:self action:@selector(preBTClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_preBT];
+        [_preBT mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(self.mas_centerY);
+            make.width.mas_equalTo(38);
+            make.height.mas_equalTo(46);
+            make.left.mas_equalTo(12);
+        }];
+    }
+    return _preBT;
+}
+
+- (void)nextBTClick:(id)sender
+{
+    CGFloat x = self.scrollView.frame.size.width * (self.currentPage+1);
+    if (x > self.scrollView.contentSize.width-self.scrollView.frame.size.width) {
+        x = self.scrollView.contentSize.width - self.scrollView.frame.size.width;
+    }
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.scrollView setContentOffset:CGPointMake(x, 0) animated:NO];
+        [self scrollViewDidEndDecelerating:self.scrollView];
+    }];
+}
+
+- (void)preBTClick:(id)sender
+{
+    CGFloat x = self.scrollView.frame.size.width * (self.currentPage-1);
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.scrollView setContentOffset:CGPointMake(x, 0) animated:NO];
+        [self scrollViewDidEndDecelerating:self.scrollView];
+    }];
+}
+
+#pragma mark - UIScrollViewDelegate M
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    NSLog(@"=====>%f",scrollView.contentOffset.x);
+    int currentPage = scrollView.contentOffset.x/scrollView.frame.size.width + ((int)scrollView.contentOffset.x%(int)scrollView.frame.size.width == 0 ? 0 : 1);
+    self.currentPage = currentPage;
+    int totalPage = ceil(scrollView.contentSize.width/scrollView.frame.size.width);
+    if (currentPage+1 == totalPage) {
+        //最后一页
+        self.nextBT.hidden = YES;
+        self.preBT.hidden = NO;
+    }else if (currentPage == 0){
+        //第一页
+        self.nextBT.hidden = NO;
+        self.preBT.hidden = YES;
+    }else{
+        //中间页
+        self.nextBT.hidden = NO;
+        self.preBT.hidden = NO;
+    }
+}
 
 @end
