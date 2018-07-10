fd32390d5539e763c948fc9a36f27faa74a1b73d
shin <shin@dawoo.com>
2018-07-10 19:06:36 +0800

    增加首页游戏列表

---
+++ GameBox/HomePage/View/SH_GameItemView.m
@@ -0,0 +1,21 @@
+//
+//  SH_GameItemView.m
+//  GameBox
+//
+//  Created by shin on 2018/7/10.
+//  Copyright © 2018年 shin. All rights reserved.
+//
+
+#import "SH_GameItemView.h"
+
+@implementation SH_GameItemView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ GameBox/HomePage/View/SH_GamesListScrollView.m
@@ -0,0 +1,21 @@
+//
+//  SH_GamesListScrollView.m
+//  GameBox
+//
+//  Created by shin on 2018/7/10.
+//  Copyright © 2018年 shin. All rights reserved.
+//
+
+#import "SH_GamesListScrollView.h"
+
+@implementation SH_GamesListScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
