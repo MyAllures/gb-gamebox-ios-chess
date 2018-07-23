24a877cff343a6637886bdbfc7d1d7aa810699a2
jun <jun@dawoo.com>
2018-07-23 17:32:16 +0800

    增加绑定手机

---
+++ GameBox/UserCenter/SaftyCenter/View/SH_FillRealNameView.h
@@ -0,0 +1,13 @@
+//
+//  SH_FillRealNameView.h
+//  GameBox
+//
+//  Created by jun on 2018/7/23.
+//  Copyright © 2018年 shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SH_FillRealNameView : UIView
+
+@end
--- GameBox/UserCenter/SaftyCenter/View/SH_ProfitExchangeView.m
+++ GameBox/UserCenter/SaftyCenter/View/SH_ProfitExchangeView.m
@@ -7,15 +7,36 @@
 //
 
 #import "SH_ProfitExchangeView.h"
+#import "SH_ProfitExchangeTableViewCell.h"
+@interface SH_ProfitExchangeView()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *mainTableView;
 
+@end
 @implementation SH_ProfitExchangeView
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)awakeFromNib{
+    [super awakeFromNib];
+    self.mainTableView.delegate = self;
+    self.mainTableView.dataSource = self;
+    [self.mainTableView registerNib:[UINib nibWithNibName:@"SH_ProfitExchangeTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"SH_ProfitExchangeTableViewCell"];
 }
-*/
+
+#pragma mark--
+#pragma mark--UITableView
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 15;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 32;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    SH_ProfitExchangeTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SH_ProfitExchangeTableViewCell"];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+- (IBAction)oneKeyRefresh:(id)sender {
+}
+- (IBAction)oneKeyRecovery:(id)sender {
+}
+
 
 @end
