4343331aa8de42747cae1e387d48b369de3536db
sam <sam@boss.test.s>
2018-07-24 21:03:22 +0800

    添加找回密码UI

---
+++ GameBox/LoginPage/View/FindPSW/SH_FindPSWSendCodeView.h
@@ -0,0 +1,13 @@
+//
+//  SH_FindPSWSendCodeView.h
+//  GameBox
+//
+//  Created by sam on 2018/7/24.
+//  Copyright © 2018年 shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SH_FindPSWSendCodeView : UIView
+
+@end
---
+++ GameBox/LoginPage/View/FindPSW/SH_FindPSWSureView.m
@@ -0,0 +1,21 @@
+//
+//  SH_FindPSWSureView.m
+//  GameBox
+//
+//  Created by sam on 2018/7/24.
+//  Copyright © 2018年 shin. All rights reserved.
+//
+
+#import "SH_FindPSWSureView.h"
+
+@implementation SH_FindPSWSureView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
