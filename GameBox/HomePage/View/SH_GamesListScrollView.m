8c1cf6528e4c43d06f53296f3e6ce1ec037515dd
sam <git@git.gbboss.com:gb/gamebox-ios-chess.git>
2018-08-29 14:44:30 +0800

    修复bug10

--- GameBox/HomePage/View/SH_GamesListScrollView.m
+++ GameBox/HomePage/View/SH_GamesListScrollView.m
@@ -169,20 +169,26 @@
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
     int currentPage = scrollView.contentOffset.x/scrollView.frame.size.width + ((int)scrollView.contentOffset.x%(int)scrollView.frame.size.width == 0 ? 0 : 1);
     self.currentPage = currentPage;
-    NSLog(@"self.currentPage==%d",(int)scrollView.contentOffset.x%(int)scrollView.frame.size.width);
+    
     int totalPage = ceil(scrollView.contentSize.width/scrollView.frame.size.width);
-    if (currentPage+1 == totalPage) {
-        //最后一页
+    NSLog(@"totalPage==%d",totalPage);
+    if (totalPage == 2) {
         self.nextBT.hidden = YES;
-        self.preBT.hidden = NO;
-    }else if (currentPage == 0){
-        //第一页
-        self.nextBT.hidden = NO;
         self.preBT.hidden = YES;
-    }else{
-        //中间页
-        self.nextBT.hidden = NO;
-        self.preBT.hidden = NO;
+    } else {
+        if (currentPage+1 == totalPage) {
+            //最后一页
+            self.nextBT.hidden = YES;
+            self.preBT.hidden = NO;
+        }else if (currentPage == 0){
+            //第一页
+            self.nextBT.hidden = NO;
+            self.preBT.hidden = YES;
+        }else{
+            //中间页
+            self.nextBT.hidden = NO;
+            self.preBT.hidden = NO;
+        }
     }
 }
 
