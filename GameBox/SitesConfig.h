6bff916e05a9a532ab7f5a46016903aa0e37412b
jun <jun@dawoo.com>
2018-08-29 11:11:02 +0800

    完善更换logo

--- GameBox/SitesConfig.h
+++ GameBox/SitesConfig.h
@@ -16,52 +16,55 @@
 
 
 #if _8l6r
-
 #define CODE    @"8l6r"
 #define S       @"6bf3b743e414045e5123fc317fb9b535"
 #define SID     @"71"
 
-
 #elif _wjbn
-
 #define CODE    @"wjbn"
 #define S       @"f0581a9da25c2476d18f7675362f596c"
 #define SID     @"21"
 
-
 #elif _7wt3
-
 #define CODE    @"7wt3"
 #define S       @"1d497c482250b85debcaff3b9099d469"
 #define SID     @"18"
 
-
 #elif _ojzk
-
 #define CODE    @"ojzk"
 #define S        @"85c362697077e64248fd5f2fbdcabae8"
 #define SID        @"7000"
 
-
 #elif _ty3a
-
 #define CODE    @"ty3a"
 #define S        @"e1576bc75aabd600d0b6b078af7a96a3"
 #define SID        @"7001"
 
-
 #elif _lcat
-
 #define CODE    @"lcat"
 #define S        @"54f9540412e52a4b43aefb0b461bf043"
 #define SID        @"7002"
 
-
 #elif _ou2t //和记棋牌
-
 #define CODE    @"ou2t"
 #define S        @"06ce11c72fc53a777bd7be3a31294e32"
 #define SID        @"7003"
+
+#elif _ksul //开元棋牌
+#define CODE    @"ksul"
+#define S        @"c2f35a1410bad50250afbc3f4d8aec6d"
+#define SID        @"7005"
+
+#elif _cilz //亿发娱乐
+#define CODE    @"cilz"
+#define S        @"b2f6a357ea67e3e4474a613701790a31"
+#define SID        @"7006"
+
+#elif _kezf //九发棋牌
+#define CODE    @"kezf"
+#define S        @"5293144a5858bc5464331f9c6ab28165"
+#define SID        @"7007"
+
 
 
 #endif
